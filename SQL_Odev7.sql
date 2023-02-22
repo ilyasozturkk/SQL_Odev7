700f714cf25662818cb29ab4ddf5712c265b9bd0
ilyasozturkk <111603714+ilyasozturkk@users.noreply.github.com>
2023-02-22 11:30:33 +0300

    Add files via upload

---
+++ SQL_Odev7.sql
@@ -0,0 +1,4 @@
+SELECT rating,title FROM film GROUP BY rating,title; --film tablosunda bulunan filmleri rating değerlerine göre gruplayınız.
+SELECT replacement_cost,COUNT(replacement_cost) FROM film GROUP BY replacement_cost HAVING COUNT(replacement_cost) >50; --film tablosunda bulunan filmleri replacement_cost sütununa göre grupladığımızda film sayısı 50 den fazla olan replacement_cost değerini ve karşılık gelen film sayısını sıralayınız.
+SELECT store_id,COUNT(store_id) FROM customer GROUP BY store_id; --customer tablosunda bulunan store_id değerlerine karşılık gelen müşteri sayılarını nelerdir? 
+SELECT country_id,COUNT(country_id) FROM city GROUP BY country_id ORDER BY country_id DESC; --city tablosunda bulunan şehir verilerini country_id sütununa göre gruplandırdıktan sonra en fazla şehir sayısı barındıran country_id bilgisini ve şehir sayısını paylaşınız.
